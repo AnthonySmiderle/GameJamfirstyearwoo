00e34eab3e8ed030662fe28e4a6ab7034a9c922b
Unknown <anthony.smiderle@uoit.net>
2019-01-25 22:39:55 -0500

    wat

---
+++ GAME JAMM/Classes/MainMenuScene.cpp
@@ -0,0 +1,119 @@
+#include "HelloWorldScene.h"
+#include "MainMenuScene.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+
+//////////// SCENE CREATOR //////////////////////////////////////////
+cocos2d::Scene* MainMenu::createScene() { return MainMenu::create(); }
+
+//////////// ENTER SCENE ////////////////////////////////////////////
+void MainMenu::onEnter() { Scene::onEnter(); }
+
+//////////// INIT MENU //////////////////////////////////////////////
+bool MainMenu::init() {
+	
+	if (!Scene::init()) return false;
+
+	director = Director::getInstance();
+
+	initMenu();
+	initMouseListener();
+	initKeyboardListener();
+
+	auto windowSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	
+
+	return true;
+}
+
+//////////// INIT MENU ITEMS ////////////////////////////////////////
+void MainMenu::initMenu() {
+	Vec2 windowSize = director->getWinSizeInPixels();
+	Vec2 origin = director->getVisibleOrigin();
+
+	background = Sprite::create("backgrounds/MainMenuBG.png");
+	background->setPosition(windowSize.x / 2.0f, windowSize.y / 2.0f);
+	background->setAnchorPoint(Vec2(0.5f, 0.5f));
+
+	title = Label::create("3AM Cooking Sim!", "fonts/Roboto/Roboto-Regular.ttf", 24);
+	title->setAnchorPoint(Vec2(0.5f, 0.5f));
+	title->setPosition(Vec2(origin.x + windowSize.x / 2.0f, origin.y + windowSize.y - title->getContentSize().height));
+	title->enableShadow();
+
+	auto playLabel = Label::create("Start Game", "fonts/Roboto/Roboto-Regular.ttf", 24);
+	auto exitLabel = Label::create("Exit Game", "fonts/Roboto/Roboto-Regular.ttf", 24);
+	exitLabel->enableShadow();
+	playLabel->enableShadow();
+
+	auto playButton = MenuItemLabel::create(playLabel, [&](Ref* sender) {
+		auto helloWorldScene = HelloWorld::createScene();
+		director->replaceScene(TransitionFade::create(2.0f, helloWorldScene));
+	});
+	auto exitButton = MenuItemLabel::create(exitLabel, [&](Ref* sender) {
+		director->end();
+	});
+
+	playButton->setPosition(0, -200);
+	exitButton->setPosition(0, -250);
+
+	menu = Menu::create(playButton, exitButton, NULL);
+
+	this->addChild(background, -100);
+	this->addChild(menu, 99);
+	this->addChild(title, 100);
+}
+
+//////////// EXIT SCENE ////////////////////////////////////////////
+void MainMenu::onExit() { Scene::onExit(); }
+
+
+//////////// MOUSE LISTENER ////////////////////////////////////////
+void MainMenu::initMouseListener() {
+	mouseListener = EventListenerMouse::create();
+
+	// Mouse down
+	mouseListener->onMouseDown = [&](Event* mEvent) {
+		EventMouse* mouseEvent = dynamic_cast<EventMouse*>(mEvent);
+
+		auto mouseClickPos = mouseEvent->getLocationInView();
+		mouseClickPos.y += 540;
+
+		auto mouseButton = mouseEvent->getMouseButton();
+	};
+
+	// Mouse up
+	mouseListener->onMouseUp = [&](Event* mEvent) {
+		EventMouse* mouseEvent = dynamic_cast<EventMouse*>(mEvent);
+
+		auto mouseUpPos = mouseEvent->getLocationInView();
+		mouseUpPos.y += 540;
+	};
+
+	// Mouse move
+	mouseListener->onMouseMove = [&](Event* mEvent) {
+		EventMouse* mouseEvent = dynamic_cast<EventMouse*>(mEvent);
+
+		auto mouseEventPos = mouseEvent->getLocationInView();
+		mouseEventPos.y += 540;
+
+		mousePosition = Vec2(mouseEventPos.x, mouseEventPos.y + 540);
+	};
+
+	// Mouse scroll
+	mouseListener->onMouseScroll = [&](Event* mEvent) {};
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(mouseListener, this);
+}
+
+//////////// KEYBOARD LISTENER ////////////////////////////////////////
+void MainMenu::initKeyboardListener() {
+	keyboardListener = EventListenerKeyboard::create();
+
+	keyboardListener->onKeyPressed = [&](EventKeyboard::KeyCode keyCode, Event* kEvent) {};
+	keyboardListener->onKeyReleased = [&](EventKeyboard::KeyCode keyCode, Event* kEvent) {};
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(keyboardListener, this);
+}
---
+++ GAME JAMM/Classes/MainMenuScene.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#ifndef __MAINMENU_SCENE_H__
+#define __MAINMENU_SCENE_H__
+
+#include "cocos2d.h"
+
+class MainMenu : public cocos2d::Scene {
+public:
+	static cocos2d::Scene* createScene();
+	
+	virtual bool init();
+	virtual void onEnter();
+	virtual void onExit();
+
+	CREATE_FUNC(MainMenu);
+
+	void initMenu();
+	void initMouseListener();
+	void initKeyboardListener();
+
+private:
+	cocos2d::Director* director;
+	
+	cocos2d::EventListenerMouse* mouseListener;
+	cocos2d::EventListenerKeyboard* keyboardListener;
+	
+	cocos2d::Vec2 mousePosition;
+	
+	cocos2d::Sprite* background;
+	cocos2d::Label* title;
+	cocos2d::Menu* menu;
+
+};
+
+#endif // __MAINMENU_SCENE_H__
